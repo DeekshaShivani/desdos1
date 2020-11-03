ba35cd0d1e7030f928fa5dfd4c8b34f5b115666e
DeekshaShivani <deekshashivani208@gmail.com>
2020-11-03 13:51:26 +0530

    syscall

---
+++ syscall/buffio.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+#include <unistd.h>
+       #include <sys/syscall.h> 
+       int main(int argc,char const * argv[])
+       {
+       printf("hello \n");
+       printf("world\n");
+       return 0;
+       }
